fdc864cf512693b1aa932000bef9f49971d11dd6
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-05-04 13:34:22 -0400

    Robert Hogan. Additional completion of projectModel get.

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 //importing
 const express = require('express');
-const action = require('./data/helpers/actionModel');
-const project = require('./data/helpers/projectModel');
 
 //server run
 const server = express();
+
+//locations
+const action = require('./data/helpers/actionModel');
+const project = require('./data/helpers/projectModel');
 
 //middleware
 server.use(express.json());
@@ -14,6 +16,51 @@ server.get('/', (req, res) => {
     console.log('Running');
     res.send('Running now!');
 })
+
+
+//########################################################################### PROJECT MODEL #############################################################################
+
+// projectmodel -- get 
+server.get('/api/projects', (req, res) => {
+    project
+    .get()
+    .then(response => {
+        res.status(200).json({ response })
+    })
+    .catch(err => {
+        res.status(500).json({ Error: err })
+    })
+})
+
+// projectmodel -- get -- id
+server.get('/api/projects/:id', (req, res) => {
+    const id = req.params.id;
+    project
+    .get(id)
+    .then(response => {
+        if(response === undefined) {
+            res.status(404).json({ message: response })
+        } else {
+            res.status(200).json({ response })
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ Error: err })
+    })
+});
+
+// projectmodel -- get -- actions
+
+server.get('/api/projects/:id/actions', (req, res) => {
+    const id = req.params.id;
+    project
+    .getProjectActions(id)
+    .then(response => {
+        res.status(200)
+    })
+    .catch()
+});
+
 
 
 
